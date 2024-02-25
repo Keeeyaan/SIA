8128961e3a87e0476f66dd7aeb02bc86412d8f54
keeeyaan <keanjieden@gmail.com>
2024-02-25 21:24:18 +0800

    format

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 import os
+
 import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
 RELOAD = bool(os.getenv('RELOAD'))
+
 if __name__ == "__main__":
     uvicorn.run("src.app:app", reload=RELOAD)

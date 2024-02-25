f2994379104565bd081bb1e9f1601ed1a60a4028
keeeyaan <keanjieden@gmail.com>
2024-02-25 21:25:54 +0800

    add initialize db on_event startup & router

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,9 +3,14 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from src.config.database import init_db
+
+# Routes Import
+from src.routes.admin_router import admin
+
 app = FastAPI(
-    title="UCnianGuideBot APIs",
-    description="These APIs encompass the entire UCnianGuideBot platform, serving both the admin portal and user portal.",
+    title="UCnian Guide Bot APIs",
+    description="These APIs encompass the entire UCnian Guide Bot platform, serving both the admin portal and user portal.",
     version="0.1.0",
 )
 
@@ -18,11 +23,9 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+@app.on_event("startup")
+async def start_db():
+    await init_db()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(admin, tags=["Admins"], prefix="/v1/admins")

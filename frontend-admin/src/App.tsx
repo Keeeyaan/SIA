664d3ec7a8794cc25c8b84d221f393bd0155f559
keeeyaan <keanjieden@gmail.com>
2024-02-29 15:31:23 +0800

    add frontend-admin

---
+++ frontend-admin/src/App.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { Error, Dashboard, Layout, Login } from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ frontend-admin/src/components/Wrapper.tsx
@@ -0,0 +1,34 @@
+import { ReactNode, useEffect } from "react";
+import { cn } from "@/lib/utils";
+
+const Wrapper = ({
+  className,
+  title,
+  norMargin,
+  children,
+}: {
+  className?: string;
+  title?: string;
+  norMargin?: boolean;
+  children: ReactNode;
+}) => {
+  useEffect(() => {
+    document.title = `GuideBot | ${title || ""}`;
+    window.scroll(0, 0);
+  }, [title]);
+
+  return (
+    <main
+      className={cn(
+        `${
+          norMargin ? "md:px-10" : "md:px-40"
+        } w-full mx-auto px-4  max-screen-xl`,
+        className
+      )}
+    >
+      {children}
+    </main>
+  );
+};
+
+export default Wrapper;

3f3151c48df1a636c79acd992de0c1a21c24c2a9
keeeyaan <keanjieden@gmail.com>
2024-02-29 15:31:23 +0800

    add frontend-admin

---
+++ frontend-admin/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Layout } from "./Layout";
+export { default as Dashboard } from "./Dashboard";
+export { default as Login } from "./Login";
+export { default as Error } from "./Error";

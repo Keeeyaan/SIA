822acfdc15a8cdd57b0942f8ff056084bddf4c38
keeeyaan <keanjieden@gmail.com>
2024-03-02 13:08:14 +0800

    added login page ui

--- frontend-admin/src/pages/Login.tsx
+++ frontend-admin/src/pages/Login.tsx
@@ -1,5 +1,36 @@
+import LoginForm from "@/components/LoginForm";
+import Wrapper from "@/components/Wrapper";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Wrapper
+      title="Login"
+      className="h-screen flex justify-center items-center"
+    >
+      <Card className="w-[400px]">
+        <CardHeader>
+          <div className="flex justify-center items-center gap-2 mb-2">
+            {/* <img src="/logo.png" className="w-[50px] h-[50px] mb-2" /> */}
+            <h1 className="font-semibold text-3xl">UCnian Guide Bot</h1>
+          </div>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Enter your email and password to login.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+      </Card>
+    </Wrapper>
+  );
 };
 
 export default Login;

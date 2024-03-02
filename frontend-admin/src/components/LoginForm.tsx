c901af4048f9d7e26cd3cc4f7a9c6c4be47ce97c
keeeyaan <keanjieden@gmail.com>
2024-03-02 13:06:31 +0800

    added login form

---
+++ frontend-admin/src/components/LoginForm.tsx
@@ -0,0 +1,78 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+// import { Loader2 } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const LoginValidationSchema = z.object({
+  email: z.string().min(1, "Email is required").email().trim(),
+  password: z
+    .string()
+    .min(6, "Password must contain at least 6 character(s)")
+    .max(128, "Password must contain at most 128 character(s)")
+    .trim(),
+});
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginValidationSchema>>({
+    resolver: zodResolver(LoginValidationSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof LoginValidationSchema>) {
+    // login(values);
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit">
+          {/* {isPending ? <Loader2 className=" animate-spin" /> : "Login"} */}
+          Login
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;

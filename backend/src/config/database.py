91ea26e460a14bef3c47f7e560465c221f169f22
keeeyaan <keanjieden@gmail.com>
2024-03-04 18:50:25 +0800

    added intent api

--- backend/src/config/database.py
+++ backend/src/config/database.py
@@ -7,9 +7,10 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from src.models.admin import Admin
 from src.models.inquiry import Inquiry
 from src.models.knowledge_base import KnowledgeBase
+from src.models.intent import Intent
 
 load_dotenv()
-MONGODB_URI = os.getenv('MONGODB_URI_LOCAL')
+MONGODB_URI = os.getenv('MONGODB_URI_ATLAS')
 MONGODB_NAME = os.getenv('MONGODB_NAME')
 
 client = AsyncIOMotorClient(MONGODB_URI)
@@ -18,8 +19,7 @@ db = client[MONGODB_NAME]
 
 async def init_db():
     """
-    The `init_db` function initializes the database and sets up the document models for various question
-    types, accounts, activities, and users.
+    The `init_db` function initializes the database and sets up the document models.
     """
     try:
         await init_beanie(
@@ -27,6 +27,7 @@ async def init_db():
             document_models=[
                 Admin,
                 Inquiry,
+                Intent,
                 KnowledgeBase
             ],
         )
---
+++ backend/src/routes/intent_router.py
@@ -0,0 +1,70 @@
+from typing import Annotated, Dict
+
+from fastapi import APIRouter, status, HTTPException
+from bson import ObjectId
+from src.models.intent import Intent
+from pydantic import BaseModel
+
+intent = APIRouter()
+
+
+class Pattern(BaseModel):
+    pattern: str
+
+
+class Response(BaseModel):
+    response: str
+
+
+@intent.get('/', status_code=status.HTTP_200_OK)
+async def get_all_intents():
+    intent = await Intent.find_all().to_list()
+    return intent
+
+
+@intent.get('/{tag}/', status_code=status.HTTP_200_OK)
+async def get_all_patterns_and_responses_by_tag(tag: str):
+    intent = await Intent.find_one(Intent.tag == tag)
+    return {"patterns": intent.patterns, "responses": intent.responses}
+
+
+@intent.post('/', status_code=status.HTTP_201_CREATED)
+async def create_intent(intent: Intent):
+    await intent.create()
+    return {"detail": "Intent created successfully!"}
+
+
+@intent.post("/pattern/{tag}/", status_code=status.HTTP_201_CREATED)
+async def add_tag_pattern(tag: str, data: Pattern):
+    try:
+        intent = await Intent.find_one(Intent.tag == tag)
+
+        if intent is None:
+            raise HTTPException(status.HTTP_404_NOT_FOUND,
+                                detail="Tag not found!")
+
+        intent.patterns.append(data.pattern.strip())
+        await intent.save()
+
+        return {"detail": "Pattern added successfully!"}
+    except Exception as e:
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
+
+@intent.post('/response/{tag}/', status_code=status.HTTP_201_CREATED)
+async def add_tag_response(tag: str, data: Response):
+    try:
+        intent = await Intent.find_one(Intent.tag == tag)
+
+        if intent is None:
+            raise HTTPException(status.HTTP_404_NOT_FOUND,
+                                detail="Tag not found!")
+
+        intent.responses.append(data.response.strip())
+        await intent.save()
+
+        return {"detail": "Response added successfully!"}
+    except Exception as e:
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")

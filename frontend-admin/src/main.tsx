07e42e32d2b81f19346ddc7a1a3f9fb9928db47b
keeeyaan <keanjieden@gmail.com>
2024-03-04 18:51:48 +0800

    added shadcn toaster component ui

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.tsx";
+import { Toaster } from "@/components/ui/toaster";
 import "./index.css";
 
 const queryClient = new QueryClient({
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>
 );

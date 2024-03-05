9f3215c85d9e37e9b7efcaa033cb9d8b1b81b8e6
gochuicod <gochuicodarelleq@gmail.com>
2024-03-05 16:09:08 +0800

    Added JWT authentication

--- backend/src/routes/authentication_router.py
+++ backend/src/routes/authentication_router.py
@@ -1,38 +1,61 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
-from dotenv import load_dotenv
 from jose import JWTError, jwt
-from os import environ
+from dotenv import load_dotenv
+from os import getenv
 
 from src.models.admin import Admin
+from src.utils import get_current_user
+
+auth = APIRouter()
 
 load_dotenv()
+ACCESS_TOKEN_EXPIRES_WEEKS = getenv("ACCESS_TOKEN_EXPIRES_WEEKS")
 
-SECRET_KEY = environ.get("SECRET_KEY")
+SECRET_KEY = getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+  return pwd_context.verify(plain_password, hashed_password)
 
+async def authenticate_user(email: str, password:str):
+  user = await Admin.find_one(Admin.email == email)
 
-async def get_user(email: str):
-    user = await Admin.find_one(Admin.email == email)
-    return Admin(user)
-
-
-def authenticate_user(email: str, password:str):
-    user = get_user(email)
-    if not user or not verify_password(password, user.password):
-        return False
-    return user
-
+  if not user or not verify_password(password, user.password):
+    return False
+  
+  return user
 
 def create_access_token(data: dict, expires_delta: timedelta):
-    to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+  to_encode = data.copy()
+  expire = datetime.utcnow() + expires_delta
+  to_encode.update({"exp": expire})
+  encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+  return encoded_jwt
+
+@auth.post('/token', status_code=status.HTTP_200_OK)
+async def login(data: dict) -> object:
+  user = await authenticate_user(data.get("email"), data.get("password"))
+  if not user:
+    HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Invalid login credentials",
+      headers={"WWW-Authenticate": "Bearer"}
+    )
+
+  access_token_expires = timedelta(weeks=int(ACCESS_TOKEN_EXPIRES_WEEKS))
+  access_token = create_access_token(
+    data={"sub": user.email},
+    expires_delta=access_token_expires
+  )
+
+  return {"access_token": access_token, "token_type": "bearer"}
+
+@auth.get('/users/me', status_code=status.HTTP_200_OK)
+async def current_user(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+  return current_user

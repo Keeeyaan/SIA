d3c091a407e4918a92e8df8318be9c6a06e19307
gochuicod <gochuicodarelleq@gmail.com>
2024-03-05 16:08:08 +0800

    Added get current user utility

---
+++ backend/src/utils.py
@@ -0,0 +1,42 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from dotenv import load_dotenv
+from os import getenv
+
+from src.models.admin import Admin
+
+security = HTTPBearer()
+
+load_dotenv()
+ACCESS_TOKEN_EXPIRES_WEEKS = getenv("ACCESS_TOKEN_EXPIRES_WEEKS")
+
+SECRET_KEY = getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> str:
+  token = credentials.credentials
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    email: str = payload.get("sub")
+    if email is None:
+      raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+      )
+  except JWTError:
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Invalid authentication credentials",
+      headers={"WWW-Authenticate": "Bearer"},
+    )
+
+  user = await Admin.find_one(Admin.email == email)
+  if user is None:
+    raise HTTPException(
+      status_code=status.HTTP_404_NOT_FOUND,
+      detail="Admin user not found"
+    )
+  
+  return user

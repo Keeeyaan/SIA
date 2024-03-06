68e5dd780cdea9589c080b69d3c0da78ae779304
keeeyaan <keanjieden@gmail.com>
2024-03-06 19:25:51 +0800

    formatted

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -12,31 +12,32 @@ load_dotenv()
 ACCESS_TOKEN_EXPIRES_WEEKS = getenv("ACCESS_TOKEN_EXPIRES_WEEKS")
 
 SECRET_KEY = getenv("SECRET_KEY")
-ALGORITHM = "HS256"
+ALGORITHM = getenv("ALGORITHM")
+
 
 async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> str:
-  token = credentials.credentials
-  try:
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    email: str = payload.get("sub")
-    if email is None:
-      raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid authentication credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-      )
-  except JWTError:
-    raise HTTPException(
-      status_code=status.HTTP_401_UNAUTHORIZED,
-      detail="Invalid authentication credentials",
-      headers={"WWW-Authenticate": "Bearer"},
-    )
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication credentials",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
-  user = await Admin.find_one(Admin.email == email)
-  if user is None:
-    raise HTTPException(
-      status_code=status.HTTP_404_NOT_FOUND,
-      detail="Admin user not found"
-    )
-  
-  return user
+    user = await Admin.find_one(Admin.email == email)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Admin user not found"
+        )
+
+    return user

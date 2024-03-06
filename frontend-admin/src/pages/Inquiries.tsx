0e6c997df6418902adef9914a3ea2d8b8c07955d
keeeyaan <keanjieden@gmail.com>
2024-03-06 19:17:50 +0800

    added inquiries page

---
+++ frontend-admin/src/pages/Inquiries.tsx
@@ -0,0 +1,11 @@
+import Wrapper from "@/components/Wrapper";
+
+const Inquiries = () => {
+  return (
+    <Wrapper title="Inquiries" norMargin>
+      Inquiries
+    </Wrapper>
+  );
+};
+
+export default Inquiries;

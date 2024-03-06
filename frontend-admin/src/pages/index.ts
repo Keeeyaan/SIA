e87e95a151378bfa8112dc15fb16090cfb57c16f
keeeyaan <keanjieden@gmail.com>
2024-03-06 19:22:46 +0800

    added pages to import & router

--- frontend-admin/src/pages/index.ts
+++ frontend-admin/src/pages/index.ts
@@ -1,4 +1,8 @@
 export { default as Layout } from "./Layout";
+export { default as ProtectedRoute } from "./ProtectedRoute";
+export { default as DashboardLayout } from "./DashboardLayout";
 export { default as Dashboard } from "./Dashboard";
+export { default as Inquiries } from "./Inquiries";
+export { default as Feedback } from "./Feedback";
 export { default as Login } from "./Login";
 export { default as Error } from "./Error";

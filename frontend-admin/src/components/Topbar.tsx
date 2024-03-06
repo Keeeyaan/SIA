20f2ad3fd2337bc90b65666a2fcaf45d9e5e2749
keeeyaan <keanjieden@gmail.com>
2024-03-06 19:23:05 +0800

    added button toggle functionality

--- frontend-admin/src/components/Topbar.tsx
+++ frontend-admin/src/components/Topbar.tsx
@@ -4,20 +4,26 @@ import UserProfile from "./UserProfile";
 import SearchInput from "./SearchInput";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
+import { useStore } from "@/store";
 
 const Topbar = () => {
-  const sidebarIsClosed = true;
+  const { sidebarIsClosed, setSidebarIsClosed } = useStore();
 
   return (
     <Card className="sticky inset-x-0 top-0 z-30 border-t-0 border-x-0 w-full rounded-none backdrop-blur-lg transition-all">
       <div className="w-full h-full flex items-center px-6 py-5 justify-between">
         <div className="flex items-center gap-2">
-          <Button variant="ghost" size="sm" className="px-2 text-gray-800">
+          <Button
+            variant="ghost"
+            size="sm"
+            className="px-2 text-gray-800"
+            onClick={setSidebarIsClosed}
+          >
             {sidebarIsClosed ? <PanelLeftOpen /> : <PanelRightOpen />}
           </Button>
           <SearchInput type="text" placeholder="Search" />
         </div>
-        <div className="flex items-center gap-6 text-gray-800">
+        <div className="flex items-center gap-4 text-gray-800">
           <div className="p-2 border rounded-full">
             <Bell size={20} />
           </div>

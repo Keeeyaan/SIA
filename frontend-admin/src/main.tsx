5ea62f7659c60186c80a9f96e1ddf2dfc7b02732
keeeyaan <keanjieden@gmail.com>
2024-03-06 19:24:22 +0800

    added custom axios interceptor

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import { Toaster } from "@/components/ui/toaster";
 import "./index.css";
+import AxiosInterceptor from "./components/AxiosInterceptor.tsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,9 +20,11 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
+    <AxiosInterceptor>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
+    </AxiosInterceptor>
   </React.StrictMode>
 );

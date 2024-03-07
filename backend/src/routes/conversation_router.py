fdbcb2193bacc9a4be1bac36fc9719aec081f4bf
keeeyaan <keanjieden@gmail.com>
2024-03-07 13:16:09 +0800

    fixed imports

--- backend/src/routes/conversation_router.py
+++ backend/src/routes/conversation_router.py
@@ -3,19 +3,22 @@ from fastapi.security import HTTPAuthorizationCredentials
 from bson import ObjectId
 
 from src.models.conversation import Conversation
-from src.utils import get_current_user
+from src.utils.user import get_current_user
 
 conversation = APIRouter()
+
 
 @conversation.get('/', status_code=status.HTTP_200_OK)
 async def get_conversations(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> Conversation:
     conversations = await Conversation.find_all().to_list()
     return conversations
 
+
 @conversation.post('/', status_code=status.HTTP_201_CREATED, response_model=Conversation)
 async def post_conversation(data: Conversation, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> Conversation:
     await data.create()
     return data
+
 
 @conversation.delete('/{id}', status_code=status.HTTP_200_OK)
 async def delete_conversation(id: str, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> object:
--- backend/src/routes/intent_router.py
+++ backend/src/routes/intent_router.py
@@ -3,65 +3,72 @@ from fastapi.security import HTTPAuthorizationCredentials
 from pydantic import BaseModel
 
 from src.models.intent import Intent
-from src.utils import get_current_user
+from src.utils.user import get_current_user
 
 intent = APIRouter()
 
+
 class Pattern(BaseModel):
-  pattern: str
+    pattern: str
+
 
 class Response(BaseModel):
-  response: str
+    response: str
+
 
 @intent.get('/', status_code=status.HTTP_200_OK)
 async def get_all_intents(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
-  intent = await Intent.find_all().to_list()
-  return intent
+    intent = await Intent.find_all().to_list()
+    return intent
+
 
 @intent.get('/{tag}/', status_code=status.HTTP_200_OK)
 async def get_all_patterns_and_responses_by_tag(tag: str, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
-  intent = await Intent.find_one(Intent.tag == tag)
-  return {"patterns": intent.patterns, "responses": intent.responses}
+    intent = await Intent.find_one(Intent.tag == tag)
+    return {"patterns": intent.patterns, "responses": intent.responses}
+
 
 @intent.post('/', status_code=status.HTTP_201_CREATED)
 async def create_intent(intent: Intent, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
-  await intent.create()
-  return {"detail": "Intent created successfully!"}
+    await intent.create()
+    return {"detail": "Intent created successfully!"}
+
 
 @intent.post("/pattern/{tag}/", status_code=status.HTTP_201_CREATED)
 async def add_tag_pattern(tag: str, data: Pattern, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
-  try:
-    intent = await Intent.find_one(Intent.tag == tag)
+    try:
+        intent = await Intent.find_one(Intent.tag == tag)
 
-    if intent is None:
-      raise HTTPException(
-        status.HTTP_404_NOT_FOUND,
-        detail="Tag not found!"
-      )
+        if intent is None:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND,
+                detail="Tag not found!"
+            )
 
-    intent.patterns.append(data.pattern.strip())
-    await intent.save()
+        intent.patterns.append(data.pattern.strip())
+        await intent.save()
 
-    return {"detail": "Pattern added successfully!"}
-  except Exception as e:
-    raise HTTPException(
-      status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+        return {"detail": "Pattern added successfully!"}
+    except Exception as e:
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+
 
 @intent.post('/response/{tag}/', status_code=status.HTTP_201_CREATED)
 async def add_tag_response(tag: str, data: Response, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
-  try:
-    intent = await Intent.find_one(Intent.tag == tag)
+    try:
+        intent = await Intent.find_one(Intent.tag == tag)
 
-    if intent is None:
-      raise HTTPException(
-        status.HTTP_404_NOT_FOUND,
-        detail="Tag not found!"
-      )
+        if intent is None:
+            raise HTTPException(
+                status.HTTP_404_NOT_FOUND,
+                detail="Tag not found!"
+            )
 
-    intent.responses.append(data.response.strip())
-    await intent.save()
+        intent.responses.append(data.response.strip())
+        await intent.save()
 
-    return {"detail": "Response added successfully!"}
-  except Exception as e:
-    raise HTTPException(
-      status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")
+        return {"detail": "Response added successfully!"}
+    except Exception as e:
+        raise HTTPException(
+            status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal Server Error")

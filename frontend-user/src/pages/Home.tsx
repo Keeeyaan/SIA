5a612de63c36482c0edfc08a9cce446ba1c1fed0
Marbuen <marbuen55@gmail.com>
2024-03-08 03:15:41 +0800

    adding the home

--- frontend-user/src/pages/Home.tsx
+++ frontend-user/src/pages/Home.tsx
@@ -1,5 +1,15 @@
+import myImage from "../assets/uclogo.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <img
+        src={myImage}
+        alt="My Image"
+        className="w-3/4 h-3/4 object-contain"
+      />
+    </div>
+  );
 };
 
 export default Home;

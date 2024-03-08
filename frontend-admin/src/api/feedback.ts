8fbdf852c32d688dd8e84dc231b21be8b1cc1d03
keeeyaan <keanjieden@gmail.com>
2024-03-08 09:34:53 +0800

    added feedback api call

---
+++ frontend-admin/src/api/feedback.ts
@@ -0,0 +1,16 @@
+import { axiosPrivate } from "@/components/AxiosInterceptor";
+
+interface IGetAllInquiriesResponse {
+  _id: string;
+  sequence: [];
+  comment: string;
+  sentiment: string;
+  version: string;
+  created_at: Date;
+}
+
+export const getAllFeedbacks = async () => {
+  const response =
+    await axiosPrivate.get<IGetAllInquiriesResponse[]>(`inquiries/`);
+  return response.data;
+};

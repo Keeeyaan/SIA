47e526c867e2222fbf23e302c0ddfe6b3a6b24f0
Keeeyaan <Keanjieden@gmail.com>
2024-03-09 12:25:43 +0800

    Added Feedback Model and Route

---
+++ backend/src/routes/feedback_router.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from bson import ObjectId
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+from src.models.feedback import Feedback
+from src.routes.inquiry_router import not_found
+from src.utils.user import get_current_user
+
+feedback = APIRouter()
+
+
+@feedback.get('/', status_code=status.HTTP_200_OK)
+async def get_feedbacks(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+    feedbacks = await Feedback.find_all().to_list()
+    return feedbacks
+
+
+@feedback.post('/', status_code=status.HTTP_201_CREATED, response_model=Feedback)
+async def post_feedback(data: Feedback, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+    await data.insert()
+    return data
+
+
+@feedback.delete('/{id}', status_code=status.HTTP_200_OK)
+async def delete_feedback(id: str, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+    feedback = await Feedback.find_one(Feedback.id == ObjectId(id))
+
+    not_found("Feedback", feedback)
+
+    await feedback.delete()
+
+    return {"detail": "Feedback deleted successfully", "deleted_feedback": feedback}

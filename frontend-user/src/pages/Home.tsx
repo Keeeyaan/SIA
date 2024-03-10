41550ed3d10360de53d0a8d70f07a225ec2d74a5
keeeyaan <keanjieden@gmail.com>
2024-03-10 23:56:05 +0800

    added bottom bar

--- frontend-user/src/pages/Home.tsx
+++ frontend-user/src/pages/Home.tsx
@@ -1,14 +1,18 @@
+import Bottombar from "@/components/Bottombar";
 import myImage from "../assets/uclogo.png";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <img
-        src={myImage}
-        alt="My Image"
-        className="w-3/4 h-3/4 object-contain"
-      />
-    </div>
+    <>
+      <div className="flex items-center justify-center">
+        <img
+          src={myImage}
+          alt="My Image"
+          className="w-3/4 h-3/4 object-contain"
+        />
+      </div>
+      <Bottombar />
+    </>
   );
 };
 

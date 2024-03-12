e1ed882c32336bfcf78ba156873c60999219262a
gochuicod <gochuicodarelleq@gmail.com>
2024-03-12 20:33:23 +0800

    Added model router

---
+++ backend/src/routes/model_router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, status, Depends
+from fastapi.security import HTTPAuthorizationCredentials
+
+from src.utils.train import init, create_model, fit_model, save_model
+from src.utils.user import get_current_user
+
+from src.models.intent import Intent
+
+model = APIRouter()
+
+@model.post('/', status_code=status.HTTP_200_OK)
+async def train_model(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+    intents = await Intent.find_all().to_list()
+
+    initial = init({'intents': intents})
+
+    model = create_model(
+        initial.get('vocabulary_size'),
+        initial.get('input_shape'),
+        initial.get('output_length')
+    )
+
+    model = fit_model(
+        model,
+        initial.get('x_train'),
+        initial.get('y_train')
+    )
+
+    save_model(model, 'version_1.0', 'keras')

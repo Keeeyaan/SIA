be70f897b105bbf092c3f47af246755509c1a452
keeeyaan <keanjieden@gmail.com>
2024-03-14 17:03:28 +0800

    added delete functionality

--- frontend-admin/src/components/SelectTagButton.tsx
+++ frontend-admin/src/components/SelectTagButton.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check, ChevronsUpDown, Loader2, X } from "lucide-react";
 
 import {
   Command,
@@ -13,10 +13,22 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Button } from "./ui/button";
 
 import { cn } from "@/lib/utils";
 import { IGetIntentsResponse } from "@/api/intents";
+import { useDeleteIntentByTag } from "@/hooks/useDeleteIntentByTag";
 
 const SelectTagButton = ({
   intents,
@@ -31,6 +43,8 @@ const SelectTagButton = ({
 }) => {
   const [open, setOpen] = useState(false);
 
+  const { mutate: deleteIntent, isPending } = useDeleteIntentByTag();
+
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
@@ -44,31 +58,60 @@ const SelectTagButton = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[300px] p-0" align="start">
+      <PopoverContent className="w-[300px] h-[500px] p-0" align="start">
         <Command>
           <CommandInput placeholder="Search tag" />
           <CommandEmpty>No intent found.</CommandEmpty>
-          <CommandGroup>
+          <CommandGroup className=" overflow-y-scroll">
             {isLoading ? (
               <p>Loading...</p>
             ) : (
               intents?.map((intent) => (
-                <CommandItem
+                <div
                   key={intent._id.toString()}
-                  value={intent.tag}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                  }}
+                  className="flex items-center justify-between gap-2"
                 >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === intent.tag ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {intent.tag}
-                </CommandItem>
+                  <CommandItem
+                    className="w-full"
+                    value={intent.tag}
+                    onSelect={(currentValue) => {
+                      setValue(currentValue === value ? "" : currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === intent.tag ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {intent.tag}
+                  </CommandItem>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant="ghost" size="sm">
+                        <X size={15} />
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                          This action cannot be undone. This will permanently
+                          remove the intent on the database.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                          onClick={() => deleteIntent(intent.tag)}
+                        >
+                          {isPending ? <Loader2 /> : "Continue"}
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
               ))
             )}
           </CommandGroup>

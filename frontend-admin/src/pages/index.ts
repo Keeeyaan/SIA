30c5e519a82e7bd011a01bf67c39b446c2c2738d
keeeyaan <keanjieden@gmail.com>
2024-03-14 17:01:41 +0800

    added accoutn page to export

--- frontend-admin/src/pages/index.ts
+++ frontend-admin/src/pages/index.ts
@@ -4,5 +4,6 @@ export { default as DashboardLayout } from "./DashboardLayout";
 export { default as Dashboard } from "./Dashboard";
 export { default as Inquiries } from "./Inquiries";
 export { default as Feedback } from "./Feedback";
+export { default as Account } from "./Account";
 export { default as Login } from "./Login";
 export { default as Error } from "./Error";

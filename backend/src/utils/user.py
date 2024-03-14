19e9cd9d1b24e57feec86d2c79bd64bb18d77e95
gochuicod <gochuicodarelleq@gmail.com>
2024-03-14 21:24:54 +0800

    Transfered utilities

--- backend/src/utils/user.py
+++ backend/src/utils/user.py
@@ -3,6 +3,8 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
 from dotenv import load_dotenv
 from os import getenv
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
 
 from src.models.admin import Admin
 
@@ -10,7 +12,7 @@ security = HTTPBearer()
 
 load_dotenv()
 ACCESS_TOKEN_EXPIRES_WEEKS = getenv("ACCESS_TOKEN_EXPIRES_WEEKS")
-
+ACCESS_TOKEN_EXPIRE_MINUTES = getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 SECRET_KEY = getenv("SECRET_KEY")
 ALGORITHM = getenv("ALGORITHM")
 
@@ -44,3 +46,27 @@ async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(s
         "user": user,
         "token": token
     }
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+async def authenticate_user(email: str, password: str):
+    user = await Admin.find_one(Admin.email == email)
+
+    if not user or not verify_password(password, user.password):
+        return False
+
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + expires_delta
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt

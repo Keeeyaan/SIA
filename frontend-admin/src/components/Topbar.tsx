f39fbdc0cd9242553dd6842d0c9427f194435323
keeeyaan <keanjieden@gmail.com>
2024-03-14 17:02:57 +0800

    remove bell & calendar icon and added bg color

--- frontend-admin/src/components/Topbar.tsx
+++ frontend-admin/src/components/Topbar.tsx
@@ -1,4 +1,4 @@
-import { Bell, Calendar, PanelLeftOpen, PanelRightOpen } from "lucide-react";
+import { PanelLeftOpen, PanelRightOpen } from "lucide-react";
 
 import UserProfile from "./UserProfile";
 import SearchInput from "./SearchInput";
@@ -10,26 +10,20 @@ const Topbar = () => {
   const { sidebarIsClosed, setSidebarIsClosed } = useStore();
 
   return (
-    <Card className="sticky inset-x-0 top-0 z-30 border-t-0 border-x-0 w-full rounded-none backdrop-blur-lg transition-all">
+    <Card className="bg-[#214E87] sticky inset-x-0 top-0 z-30 border-t-0 border-x-0 w-full rounded-none backdrop-blur-lg transition-all">
       <div className="w-full h-full flex items-center px-6 py-5 justify-between">
         <div className="flex items-center gap-2">
           <Button
             variant="ghost"
             size="sm"
-            className="px-2 text-gray-800"
+            className="px-2 text-white"
             onClick={setSidebarIsClosed}
           >
             {sidebarIsClosed ? <PanelLeftOpen /> : <PanelRightOpen />}
           </Button>
           <SearchInput type="text" placeholder="Search" />
         </div>
-        <div className="flex items-center gap-4 text-gray-800">
-          <div className="p-2 border rounded-full">
-            <Bell size={20} />
-          </div>
-          <div className="p-2 border rounded-full">
-            <Calendar size={20} />
-          </div>
+        <div className="text-gray-800">
           <UserProfile />
         </div>
       </div>

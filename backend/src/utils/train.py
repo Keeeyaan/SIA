112f3fccc17ed1a1540667524e44eba859b7d404
keeeyaan <keanjieden@gmail.com>
2024-03-14 22:29:03 +0800

    fixed no module tokenizer

--- backend/src/utils/train.py
+++ backend/src/utils/train.py
@@ -3,7 +3,6 @@ from keras.callbacks import EarlyStopping
 from keras.optimizers import Adam
 from keras.utils import pad_sequences
 from keras.models import Sequential
-from keras.preprocessing.text import Tokenizer
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -11,24 +10,25 @@ import tensorflow as tf
 import pandas as pd
 import os
 
+
 def init(data: object):
     # Creating the Model
     # getting all the data to lists
     tags = []
     inputs = []
-    responses={}
+    responses = {}
 
     for intent in data['intents']:
-      responses[intent.tag] = intent.responses
+        responses[intent.tag] = intent.responses
 
-      for lines in intent.patterns:
-        inputs.append(lines)
-        tags.append(intent.tag)
-        
+        for lines in intent.patterns:
+            inputs.append(lines)
+            tags.append(intent.tag)
+
     # converting to dataframe
-    data = pd.DataFrame({"inputs":inputs, "tags":tags})
+    data = pd.DataFrame({"inputs": inputs, "tags": tags})
 
-    tokenizer = Tokenizer(num_words=2000)
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=2000)
     tokenizer.fit_on_texts(data['inputs'])
     train = tokenizer.texts_to_sequences(data['inputs'])
 
@@ -42,14 +42,14 @@ def init(data: object):
     output_length = le.classes_.shape[0]
 
     return {
-      "input_shape": input_shape,
-      "responses": responses,
-      "vocabulary_size": vocabulary_size,
-      "output_length": output_length,
-      "x_train": x_train,
-      "y_train": y_train,
-      "tokenizer": tokenizer,
-      "label_encoder": le,
+        "input_shape": input_shape,
+        "responses": responses,
+        "vocabulary_size": vocabulary_size,
+        "output_length": output_length,
+        "x_train": x_train,
+        "y_train": y_train,
+        "tokenizer": tokenizer,
+        "label_encoder": le,
     }
 
 
@@ -60,22 +60,25 @@ def create_model(vocabulary_size, input_shape, output_length):
     model.add(Bidirectional(LSTM(64, return_sequences=True)))
     model.add(Bidirectional(LSTM(64)))
     model.add(Dense(output_length, activation='softmax'))
-    
-    model.compile(loss="sparse_categorical_crossentropy", optimizer=Adam(lr=0.001), metrics=['accuracy'])
-    
+
+    model.compile(loss="sparse_categorical_crossentropy",
+                  optimizer=Adam(lr=0.001), metrics=['accuracy'])
+
     model.summary()
 
     return model
-  
+
 
 def fit_model(model, x_train, y_train):
-    early_stopping = EarlyStopping(monitor="loss", patience=10, restore_best_weights=True)
-    model.fit(x_train, y_train, epochs=1000, verbose=1, callbacks=[early_stopping])
+    early_stopping = EarlyStopping(
+        monitor="loss", patience=10, restore_best_weights=True)
+    model.fit(x_train, y_train, epochs=1000,
+              verbose=1, callbacks=[early_stopping])
 
     return model
 
 
 def save_model(model, filename, extension):
-  current_working_directory = os.path.dirname(os.path.abspath(__file__))
+    current_working_directory = os.path.dirname(os.path.abspath(__file__))
 
-  model.save(f"{current_working_directory}/versions/{filename}.{extension}")
+    model.save(f"{current_working_directory}/versions/{filename}.{extension}")

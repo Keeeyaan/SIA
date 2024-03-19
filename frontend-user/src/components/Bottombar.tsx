c263280eb87263ca48132d9ba4a05a4519fb120a
keeeyaan <keanjieden@gmail.com>
2024-03-19 17:08:43 +0800

    update bottom bar

--- frontend-user/src/components/Bottombar.tsx
+++ frontend-user/src/components/Bottombar.tsx
@@ -1,25 +1,52 @@
-import { Textarea } from "./ui/textarea";
-import { Send } from "lucide-react";
+import { useRef } from "react";
+import { Loader2, Send } from "lucide-react";
+
 import { Button } from "./ui/button";
-import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import useCreateInquiry from "@/hooks/useCreateInquiry";
 
 const Bottombar = () => {
+  const inquiryRef = useRef<HTMLInputElement>();
+
+  const { mutate: createInquiry, isPending } = useCreateInquiry();
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const inquiryValue = inquiryRef.current?.value;
+    if (!inquiryValue) {
+      return;
+    }
+    const data = { inquiry: inquiryValue };
+    // createInquiry(data);
+    console.log(data);
+  };
+
   return (
-    <div className="flex flex-col items-start justify-center left-0 z-50 h-32 w-full border-gray-200 bg-blue-500 dark:border-gray-600 dark:bg-gray-700">
-      <div className="flex items-center justify-center w-full">
-        <Textarea
-          className="mt-2 h-12 w-3/4 py-4 px-4 rounded-2xl overflow-auto"
-          placeholder="Ask any question here"
-        ></Textarea>
-        <Button className="flex items-center bg-white hover:bg-blue-200 ml-4 mt-2 rounded-full">
-          <Send color="black"></Send>
-        </Button>
+    <div className="flex flex-col justify-center w-full bg-[#214E87]">
+      <div className="p-6">
+        <form
+          onSubmit={onSubmitHandler}
+          className="flex justify-center items-center w-full gap-4"
+        >
+          <Input
+            ref={inquiryRef}
+            className="p-6 w-3/4 rounded-full"
+            placeholder="Ask any question here"
+          />
+          <Button type="submit" className="rounded-full" variant="outline">
+            {isPending ? (
+              <Loader2 className="animate-spin" />
+            ) : (
+              <Send size={18} />
+            )}
+          </Button>
+        </form>
+        <p className="text-xs text-white mt-3 text-center">
+          SIA is still in development, and while we strive for accuracy, there
+          may be times when it produces incomplete or inaccurate responses.
+          Thank you for understanding!
+        </p>
       </div>
-      <Label className="text-xs mt-3 ml-24 text-right w-3/4">
-        SIA is still in development, and while we strive for accuracy, there may
-        be times when it produces incomplete or inaccurate responses. Thank you
-        for understanding!
-      </Label>
     </div>
   );
 };

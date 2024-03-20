7bdebedd79f8d4e8bc81075f94909f003342c31c
gochuicod <gochuicodarelleq@gmail.com>
2024-03-20 10:22:27 +0800

    Temporarily disabling model functionality

--- backend/src/routes/conversation_router.py
+++ backend/src/routes/conversation_router.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials
 from bson import ObjectId
+from typing import List
 
 from src.models.conversation import Conversation, Sequence, UpdateConversation, PostConversation
 from src.models.intent import Intent
 from src.utils.user import get_current_user, create_access_token
 from src.utils.user import ACCESS_TOKEN_EXPIRES_WEEKS
 
-from src.utils.model import load_model, chatbot_respond
-from src.utils.train import init
+# from src.utils.model import load_model, chatbot_respond
+# from src.utils.train import init
 
 from datetime import datetime, timedelta
 from uuid import uuid4
@@ -16,7 +17,7 @@ from uuid import uuid4
 conversation = APIRouter()
 
 @conversation.get('/', status_code=status.HTTP_200_OK)
-async def get_all_conversations(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> Conversation:
+async def get_all_conversations(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> List[Conversation]:
     conversations = await Conversation.find_all().to_list()
     return conversations
 
@@ -27,91 +28,91 @@ async def get_conversation(token: str):
     return conversation
 
 
-@conversation.post('/', status_code=status.HTTP_201_CREATED, response_model=Conversation)
-async def post_conversation(sequence: PostConversation) -> Conversation:
-    access_token_expires = timedelta(weeks=int(ACCESS_TOKEN_EXPIRES_WEEKS))
+# @conversation.post('/', status_code=status.HTTP_201_CREATED, response_model=Conversation)
+# async def post_conversation(sequence: PostConversation) -> Conversation:
+#     access_token_expires = timedelta(weeks=int(ACCESS_TOKEN_EXPIRES_WEEKS))
     
-    token = create_access_token(
-        {"sub": str(uuid4())},
-        expires_delta=access_token_expires
-    )
+#     token = create_access_token(
+#         {"sub": str(uuid4())},
+#         expires_delta=access_token_expires
+#     )
 
-    intents = await Intent.find_all().to_list()
+#     intents = await Intent.find_all().to_list()
 
-    initial = init({'intents': intents})
+#     initial = init({'intents': intents})
 
-    try:
-      model = load_model(
-        sequence.filename,
-        sequence.extension
-      )
+#     try:
+#       model = load_model(
+#         sequence.filename,
+#         sequence.extension
+#       )
 
-      response = chatbot_respond(
-          sequence.inquiry,
-          model,
-          initial.get('tokenizer'),
-          initial.get('input_shape'),
-          initial.get('label_encoder'),
-          initial.get('responses')
-      )
+#       response = chatbot_respond(
+#           sequence.inquiry,
+#           model,
+#           initial.get('tokenizer'),
+#           initial.get('input_shape'),
+#           initial.get('label_encoder'),
+#           initial.get('responses')
+#       )
 
-      data = Conversation(
-          token=token,
-          sequence=[Sequence(
-              inquiry=sequence.inquiry,
-              response=response,
-              createdAt=datetime.now()
-          )]
-      )
+#       data = Conversation(
+#           token=token,
+#           sequence=[Sequence(
+#               inquiry=sequence.inquiry,
+#               response=response,
+#               createdAt=datetime.now()
+#           )]
+#       )
 
-      await data.create()
+#       await data.create()
 
-      return data
-    except:
-      raise HTTPException(
-          status_code=400,
-          detail="Please use the latest version of the model."
-      )
+#       return data
+#     except:
+#       raise HTTPException(
+#           status_code=400,
+#           detail="Please use the latest version of the model."
+#       )
 
 
-@conversation.patch('/', status_code=status.HTTP_200_OK)
-async def update_conversation(data: UpdateConversation) -> dict:
-    conversation = await Conversation.find_one(Conversation.token == data.token)
-    intents = await Intent.find_all().to_list()
+# @conversation.patch('/', status_code=status.HTTP_200_OK)
+# async def update_conversation(data: UpdateConversation) -> dict:
+#     conversation = await Conversation.find_one(Conversation.token == data.token)
+#     intents = await Intent.find_all().to_list()
 
-    initial = init({'intents': intents})
+#     initial = init({'intents': intents})
 
-    try:
-      model = load_model(
-          data.filename,
-          data.extension
-      )
+#     try:
+#       model = load_model(
+#           data.filename,
+#           data.extension
+#       )
 
-      response = chatbot_respond(
-          data.inquiry,
-          model,
-          initial.get('tokenizer'),
-          initial.get('input_shape'),
-          initial.get('label_encoder'),
-          initial.get('responses')
-      )
+#       response = chatbot_respond(
+#           data.inquiry,
+#           model,
+#           initial.get('tokenizer'),
+#           initial.get('input_shape'),
+#           initial.get('label_encoder'),
+#           initial.get('responses')
+#       )
 
-      conversation.sequence.append(
-        Sequence(
-            inquiry=data.inquiry,
-            response=response,
-            createdAt=datetime.now()
-        )
-      )
+#       conversation.sequence.append(
+#         Sequence(
+#             inquiry=data.inquiry,
+#             response=response,
+#             createdAt=datetime.now()
+#         )
+#       )
 
-      await conversation.save()
+#       await conversation.save()
 
-      return {'detail': conversation}
-    except:
-      raise HTTPException(
-          status_code=400,
-          detail="Please use the latest version of the model."
-      )
+#       return {'detail': conversation}
+#     except:
+#       raise HTTPException(
+#           status_code=400,
+#           detail="Please use the latest version of the model."
+#       )
 
 
 

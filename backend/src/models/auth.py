dafcd2e88c8764fdaf978fb7a195bad9de0f20ee
keeeyaan <keanjieden@gmail.com>
2024-03-20 16:40:51 +0800

    removed config

--- backend/src/models/auth.py
+++ backend/src/models/auth.py
@@ -1,14 +1,6 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 
 class LoginRequestBody(BaseModel):
     email: str
     password: str
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "email": "testuser@test.com",
-                "password": "Test1234"
-            }
-        }

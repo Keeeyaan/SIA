3a98288e9303bb4b55bdb1de8193c4296cd6b857
keeeyaan <keanjieden@gmail.com>
2024-03-22 16:47:57 +0800

    removed all unnecessary packages to make crud work

--- backend/src/routes/admin_router.py
+++ backend/src/routes/admin_router.py
@@ -11,7 +11,7 @@ admin = APIRouter()
 
 
 @admin.get('/', status_code=status.HTTP_200_OK)
-async def get_all_admins() -> List[Admin]:
+async def get_all_admins(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
     admins = await Admin.find_all().to_list()
     return admins
 

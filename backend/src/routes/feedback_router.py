2fcb1c1f32dd1869e88d7b082ca011c15b714794
gochuicod <gochuicodarelleq@gmail.com>
2024-03-23 11:45:53 +0800

    Added response type and model

--- backend/src/routes/feedback_router.py
+++ backend/src/routes/feedback_router.py
@@ -1,6 +1,7 @@
+from fastapi.security import HTTPAuthorizationCredentials
 from fastapi import APIRouter, status, Depends
 from bson import ObjectId
-from fastapi.security import HTTPAuthorizationCredentials
+from typing import List
 
 from src.models.feedback import Feedback
 from src.routes.inquiry_router import not_found
@@ -11,13 +12,13 @@ feedback = APIRouter()
 
 
 @feedback.get('/', status_code=status.HTTP_200_OK)
-async def get_feedbacks(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+async def get_feedbacks(current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> List[Feedback]:
     feedbacks = await Feedback.find_all().to_list()
     return feedbacks
 
 
-@feedback.post('/', status_code=status.HTTP_201_CREATED)
-async def post_feedback(data: Feedback):
+@feedback.post('/', status_code=status.HTTP_201_CREATED, response_model=Feedback)
+async def post_feedback(data: Feedback) -> Feedback:
     # sentiment = analyze_sentiment(data.comment) #comment this on push
 
     # data.sentiment = sentiment #comment this on push
@@ -28,7 +29,7 @@ async def post_feedback(data: Feedback):
 
 
 @feedback.delete('/{id}', status_code=status.HTTP_200_OK)
-async def delete_feedback(id: str, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)):
+async def delete_feedback(id: str, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> dict:
     feedback = await Feedback.find_one(Feedback.id == ObjectId(id))
 
     not_found("Feedback", feedback)

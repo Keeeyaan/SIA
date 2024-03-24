2dfb4056bcec2134c60145bb39222b0e31f1d636
gochuicod <gochuicodarelleq@gmail.com>
2024-03-24 22:28:40 +0800

    Updated conversation layout

--- frontend-user/src/components/Conversation.tsx
+++ frontend-user/src/components/Conversation.tsx
@@ -1,18 +1,33 @@
+import useFetchConversation from "@/hooks/useFetchConversation";
 import Chatbox from "./ChatBox";
+import { useStore } from "@/store";
 
 const Conversation = () => {
+  const { data: convo } = useFetchConversation()
+  const { inquiry, conversationPending } = useStore()
+
   return (
-    <div className="h-full p-10 space-x-[500px] flex flex-col space-y-4">
-      <Chatbox type="inquiry" />
-      <Chatbox type="response" />
-      {/* <Chatbox type="inquiry" />
-      <Chatbox type="response" />
-      <Chatbox type="inquiry" />
-      <Chatbox type="response" />
-      <Chatbox type="inquiry" />
-      <Chatbox type="response" />
-      <Chatbox type="response" />
-      <Chatbox type="response" /> */}
+    <div className="min-h-full min-w-full flex flex-col p-20 gap-y-10">
+      {
+        convo?.data?.sequence?.map((item: any) => {
+          return (
+            <div className="flex flex-col justify-between" key={item.createdAt + item.inquiry}>
+              <Chatbox type="inquiry" data={item.inquiry}/>
+              <Chatbox type="response" data={item.response}/>
+            </div>
+          )
+        })
+      }
+      {
+        conversationPending && (
+          <>
+            <div className="flex flex-col justify-between">
+              <Chatbox type="inquiry" data={inquiry}/>
+              <Chatbox type="response" data={"..."}/>
+            </div>
+          </>
+        )
+      }
     </div>
   );
 };

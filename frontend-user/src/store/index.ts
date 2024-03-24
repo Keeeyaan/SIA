3474f212c7993ac7649ed2fa551478a7b5cf2797
gochuicod <gochuicodarelleq@gmail.com>
2024-03-24 22:28:01 +0800

    Added conversation global store

--- frontend-user/src/store/index.ts
+++ frontend-user/src/store/index.ts
@@ -3,10 +3,18 @@ import { create } from "zustand";
 interface GlobalStore {
   sidebarIsClosed: boolean;
   setSidebarIsClosed: () => void;
+  conversationPending: boolean;
+  setConversationPending: (isPending: boolean) => void;
+  inquiry: string;
+  setInquiry: (value: string) => void;
 }
 
 export const useStore = create<GlobalStore>((set) => ({
   sidebarIsClosed: false,
   setSidebarIsClosed: () =>
     set((state) => ({ sidebarIsClosed: !state.sidebarIsClosed })),
+  conversationPending: false,
+  setConversationPending: (isPending: boolean) => set({ conversationPending: isPending }),
+  inquiry: "",
+  setInquiry: (value: string) => set({ inquiry: value })
 }));

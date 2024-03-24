d098c83fa715f846ca13d011546816678336daeb
gochuicod <gochuicodarelleq@gmail.com>
2024-03-24 23:07:53 +0800

    Added some animation

---
+++ frontend-user/src/components/Typewriter.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+const Typewriter = ({ text, delay, infinite }: { text: string, delay: number, infinite: boolean }) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  console.log(currentText);
+  
+
+  useEffect(() => {
+    let timeout: number | NodeJS.Timeout;
+
+    if (currentIndex < text.length) {
+      timeout = setTimeout(() => {
+        setCurrentText(prevText => prevText + text[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+
+    } else if (infinite) { // ADD THIS CHECK
+      setCurrentIndex(0);
+      setCurrentText('');
+    }
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex, delay, infinite, text]);
+
+  return <h1 className="text-sm">{currentText}</h1>;
+};
+
+export default Typewriter;

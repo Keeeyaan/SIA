7e74944b1baeb073f4006af432c98eb4eed6438b
keeeyaan <keanjieden@gmail.com>
2024-03-26 19:46:55 +0800

    update inquiry table column

--- frontend-admin/src/components/inquiry-table/columns.tsx
+++ frontend-admin/src/components/inquiry-table/columns.tsx
@@ -1,9 +1,13 @@
+import { format } from "date-fns";
 import { ColumnDef } from "@tanstack/react-table";
+import InquiryTableAction from "./InquiryTableAction";
 
 interface IInquiry {
   _id: string;
+  token: string;
   inquiry: string;
   tag: string;
+  created_at: Date;
 }
 
 export const columns: ColumnDef<IInquiry>[] = [
@@ -12,11 +16,29 @@ export const columns: ColumnDef<IInquiry>[] = [
     header: "ID",
   },
   {
+    accessorKey: "token",
+    header: "Token",
+  },
+  {
     accessorKey: "inquiry",
     header: "Inquiry",
   },
   {
     accessorKey: "tag",
     header: "Tag",
+  },
+  {
+    accessorKey: "created_at",
+    header: "Date Created",
+    cell: ({ row }) => {
+      const formattedDate = format(row.getValue("created_at"), "MMMM dd, yyyy");
+      return formattedDate;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return <InquiryTableAction inquiry={row.original} />;
+    },
   },
 ];

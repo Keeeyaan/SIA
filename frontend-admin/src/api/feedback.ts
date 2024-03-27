abcee69afa85df3b7b885804ced8f02a35a3d004
keeeyaan <keanjieden@gmail.com>
2024-03-27 14:07:17 +0800

    added create & delete api call

--- frontend-admin/src/api/feedback.ts
+++ frontend-admin/src/api/feedback.ts
@@ -1,16 +1,30 @@
 import { axiosPrivate } from "@/components/AxiosInterceptor";
+import { IGenericResponse } from "./intents";
 
-interface IGetAllInquiriesResponse {
+export interface IGetAllFeedbackResponse {
   _id: string;
   sequence: [];
   comment: string;
   sentiment: string;
-  version: string;
+  version?: string;
   created_at: Date;
 }
 
 export const getAllFeedbacks = async () => {
   const response =
-    await axiosPrivate.get<IGetAllInquiriesResponse[]>(`feedbacks/`);
+    await axiosPrivate.get<IGetAllFeedbackResponse[]>(`feedbacks/`);
+  return response.data;
+};
+
+export const createFeedback = async () => {
+  const response =
+    await axiosPrivate.post<IGetAllFeedbackResponse>(`feedbacks/`);
+  return response.data;
+};
+
+export const deleteFeedbackById = async (id: string) => {
+  const response = await axiosPrivate.delete<IGenericResponse>(
+    `feedbacks/${id}`
+  );
   return response.data;
 };

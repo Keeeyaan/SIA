e92b0704c1d57f59a51565c2293b467f51dc8dcb
gochuicod <gochuicodarelleq@gmail.com>
2024-03-28 10:02:36 +0800

    Added feedback functionality and design updates

--- frontend-user/src/components/Conversation.tsx
+++ frontend-user/src/components/Conversation.tsx
@@ -1,18 +1,17 @@
 import useFetchConversation from "@/hooks/useFetchConversation";
 import Chatbox from "./ChatBox";
-// import { useStore } from "@/store";
 
 const Conversation = () => {
   const { data: convo } = useFetchConversation()
 
   return (
-    <div className="min-h-full min-w-full flex flex-col p-20 gap-y-10">
+    <div className="min-h-full min-w-full flex flex-col pt-20 px-20">
       {
         convo?.data?.sequence?.map((item: any, index: number) => {
           return (
-            <div className="flex flex-col justify-between" key={item.createdAt + item.inquiry}>
-              <Chatbox type="inquiry" data={item.inquiry} current_index={index} length={convo?.data?.sequence.length}/>
-              <Chatbox type="response" data={item.response} current_index={index} length={convo?.data?.sequence.length}/>
+            <div className="flex flex-col justify-between gap-y-10" key={item.createdAt + item.inquiry}>
+              <Chatbox type="inquiry" data={item} current_index={index} length={convo?.data?.sequence.length}/>
+              <Chatbox type="response" data={item} current_index={index} length={convo?.data?.sequence.length}/>
             </div>
           )
         })

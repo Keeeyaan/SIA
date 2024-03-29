4c5bea9db857514f39a6771cd8f77ace61e0c043
keeeyaan <keanjieden@gmail.com>
2024-03-29 21:28:35 +0800

    added icons on link navigation

--- frontend-admin/src/components/Sidebar.tsx
+++ frontend-admin/src/components/Sidebar.tsx
@@ -1,6 +1,14 @@
+import { Link, useLocation } from "react-router-dom";
+import {
+  Bot,
+  BrainCog,
+  CircleUser,
+  MessageCircleQuestion,
+  MessageSquareText,
+} from "lucide-react";
+
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Separator } from "./ui/separator";
-import { Link } from "react-router-dom";
 import { buttonVariants } from "./ui/button";
 import { useStore } from "@/store";
 
@@ -24,12 +32,12 @@ const SmallSB = () => {
   );
 };
 
-const BigSB = () => {
+const BigSB = ({ pathname }: { pathname: string }) => {
   return (
     <div>
-      <CardHeader className="p-5">
+      <CardHeader className="py-10">
         <div className="flex items-center text-center justify-center">
-          <CardTitle>Ucnian Guide Bot Admin</CardTitle>
+          <CardTitle className="font-bold">Ucnian Guide Bot Admin</CardTitle>
         </div>
       </CardHeader>
       <Separator className="mb-4" />
@@ -37,27 +45,48 @@ const BigSB = () => {
         <div className="space-y-2">
           <Link
             to="/"
-            className={`${buttonVariants({ variant: "ghost" })} w-full rounded-none`}
+            className={`${buttonVariants({ variant: `${pathname === "/" ? "secondary" : "ghost"}`, size: "lg" })} w-full rounded-none`}
           >
-            Knowledge Base
+            <span className="flex items-center gap-4 w-full">
+              <Bot size={24} />
+              Model
+            </span>
+          </Link>
+          <Link
+            to="/intents"
+            className={`${buttonVariants({ variant: `${pathname === "/intents" ? "secondary" : "ghost"}`, size: "lg" })} w-full rounded-none`}
+          >
+            <span className="flex items-center gap-4 w-full">
+              <BrainCog />
+              Knowledge Base
+            </span>
           </Link>
           <Link
             to="/inquiries"
-            className={`${buttonVariants({ variant: "ghost" })} w-full rounded-none`}
+            className={`${buttonVariants({ variant: `${pathname === "/inquiries" ? "secondary" : "ghost"}`, size: "lg" })} w-full rounded-none`}
           >
-            Inquiries
+            <span className="flex items-center gap-4 w-full">
+              <MessageCircleQuestion />
+              Inquiries
+            </span>
           </Link>
           <Link
             to="/feedback"
-            className={`${buttonVariants({ variant: "ghost" })} w-full rounded-none`}
+            className={`${buttonVariants({ variant: `${pathname === "/feedback" ? "secondary" : "ghost"}`, size: "lg" })} w-full rounded-none`}
           >
-            Feedback
+            <span className="flex items-center gap-4 w-full">
+              <MessageSquareText />
+              Feedback
+            </span>
           </Link>
           <Link
             to="/account"
-            className={`${buttonVariants({ variant: "ghost" })} w-full rounded-none`}
+            className={`${buttonVariants({ variant: `${pathname === "/account" ? "secondary" : "ghost"}`, size: "lg" })} w-full rounded-none`}
           >
-            Account
+            <span className="flex items-center gap-4 w-full">
+              <CircleUser />
+              Account
+            </span>
           </Link>
         </div>
       </CardContent>
@@ -67,6 +96,7 @@ const BigSB = () => {
 
 const Sidebar = () => {
   const { sidebarIsClosed } = useStore();
+  const { pathname } = useLocation();
 
   return (
     <Card className="rounded-none">
@@ -75,7 +105,7 @@ const Sidebar = () => {
           sidebarIsClosed ? "w-[80px]" : "w-[280px]"
         } sticky transition-all ease-in-out duration-300 top-0`}
       >
-        {sidebarIsClosed ? <SmallSB /> : <BigSB />}
+        {sidebarIsClosed ? <SmallSB /> : <BigSB pathname={pathname} />}
       </div>
     </Card>
   );

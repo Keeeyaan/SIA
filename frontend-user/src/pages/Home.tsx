9c63b182ba0ea07d25462d9ce192ac57e8ae62ac
keeeyaan <keanjieden@gmail.com>
2024-04-04 23:13:20 +0800

    added fetch conversation

--- frontend-user/src/pages/Home.tsx
+++ frontend-user/src/pages/Home.tsx
@@ -1,16 +1,21 @@
 import Bottombar from "@/components/Bottombar";
 import Conversation from "@/components/Conversation";
 import InitialMessage from "@/components/InitialMessage";
+import useFetchConversation from "@/hooks/useFetchConversation";
 
 const Home = () => {
-  const data = [{}];
+  const { data: convo } = useFetchConversation();
 
   return (
     <>
       <div
-        className={`${data.length && "overflow-y-scroll"} flex items-center justify-center h-full w-full bg-no-repeat bg-center bg-[url('/uclogo.png')] bg-[#17AEDA] bg-opacity-20`}
+        className={`${convo?.sequence.length && "overflow-y-scroll"} flex items-center justify-center h-full w-full bg-no-repeat bg-center bg-[url('/uclogo.png')] bg-[#17AEDA] bg-opacity-20`}
       >
-        {data.length ? <Conversation /> : <InitialMessage />}
+        {convo?.sequence.length ? (
+          <Conversation convo={convo} />
+        ) : (
+          <InitialMessage />
+        )}
       </div>
       <Bottombar />
     </>

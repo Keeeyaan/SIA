83707c01ca3645d5b5581043b8eede867cd6a82a
keeeyaan <keanjieden@gmail.com>
2024-04-09 22:11:31 +0800

    update knowledge base intents field as optional

--- backend/src/models/knowledge_base.py
+++ backend/src/models/knowledge_base.py
@@ -1,14 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from beanie import Document
 from datetime import datetime
 from typing import List, Optional
 
 from src.models.intent import Intent
 
+
 class KnowledgeBase(Document):
-    intents: List[Intent]
-    version: Optional[str] = "TBD"
-    updatedAt: Optional[datetime] = datetime.now()
+    intents: Optional[List[Intent]] = []
+    version: str
+    created_at: Optional[datetime] = datetime.now()
 
     class Settings:
         name = "kbs"
@@ -17,19 +18,19 @@ class KnowledgeBase(Document):
         json_schema_extra = {
             "example": {
                 "intents": [
-                  {
-                      "tag": "greeting",
-                      "patterns": [
-                          "hello",
-                          "hi there",
-                          "nice to meet you"
-                      ],
-                      "responses": [
-                          "Hello there!",
-                          "Hi, how can I help you?"
-                      ],
-                      "frequency": 0
-                  }
+                    {
+                        "tag": "greeting",
+                        "patterns": [
+                            "hello",
+                            "hi there",
+                            "nice to meet you"
+                        ],
+                        "responses": [
+                            "Hello there!",
+                            "Hi, how can I help you?"
+                        ],
+                        "frequency": 0
+                    }
                 ],
                 "version": "1.0",
                 "updatedAt": "2024-02-29 17:59:56.071409"

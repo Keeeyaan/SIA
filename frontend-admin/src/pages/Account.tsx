29c21b1fa3fc7dab314ab65bfdf2e939a7cdd19b
keeeyaan <keanjieden@gmail.com>
2024-04-09 22:06:11 +0800

    added create account button

--- frontend-admin/src/pages/Account.tsx
+++ frontend-admin/src/pages/Account.tsx
@@ -1,9 +1,11 @@
 import AccountTable from "@/components/account-table/AccountTable";
+import CreateAccountButton from "@/components/account-table/CreateAccountButton";
 import Wrapper from "@/components/Wrapper";
 
 const Account = () => {
   return (
-    <Wrapper title="Account" norMargin>
+    <Wrapper title="Account" norMargin className="space-y-4">
+      <CreateAccountButton />
       <AccountTable />
     </Wrapper>
   );

56e0648d9cf42b4a37e4609b696f20c96405d0f2
keeeyaan <keanjieden@gmail.com>
2024-04-09 22:05:25 +0800

    added isLoading state & default value

--- frontend-admin/src/components/StatCard.tsx
+++ frontend-admin/src/components/StatCard.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import { Loader2 } from "lucide-react";
 
 import { Card } from "./ui/card";
 
@@ -7,17 +8,25 @@ const StatCard = ({
   value,
   icon,
   color,
+  isLoading,
 }: {
   title: string;
-  value: string | number;
+  value?: string | number;
   icon: ReactNode;
   color?: string;
+  isLoading?: boolean;
 }) => {
   return (
     <Card className="p-4 w-[300px] flex items-center justify-between">
       <div className="">
-        <h1 className="text-muted-foreground">{title}</h1>
-        <p className="text-xl font-medium">{value}</p>
+        {isLoading ? (
+          <Loader2 className=" animate-spin" />
+        ) : (
+          <>
+            <h1 className="text-muted-foreground">{title}</h1>
+            <p className="text-xl font-medium">{value ? value : 0}</p>
+          </>
+        )}
       </div>
       <div
         className={`${color ? color : "bg-green-500"} rounded-full w-[60px] h-[60px] flex items-center justify-center text-white`}

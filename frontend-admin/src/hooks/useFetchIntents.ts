d492deec9247ea560de87321be539d61dd172730
keeeyaan <keanjieden@gmail.com>
2024-04-11 01:10:46 +0800

    added condition for return data

--- frontend-admin/src/hooks/useFetchIntents.ts
+++ frontend-admin/src/hooks/useFetchIntents.ts
@@ -2,10 +2,14 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getIntents } from "@/api/intents";
 
-const useFetchIntents = () => {
+const useFetchIntents = (filter?: boolean) => {
   return useQuery({
     queryKey: ["intents"],
     queryFn: getIntents,
+    select: (data) => {
+      const filteredData = data.filter((intent) => intent.frequency > 0);
+      return filter ? filteredData : data;
+    },
   });
 };
 

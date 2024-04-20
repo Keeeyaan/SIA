9631c0e479e2d7282c558d89bb9d464ffd14d492
keeeyaan <keanjieden@gmail.com>
2024-04-21 00:39:32 +0800

    fixed error, and update ui

--- frontend-user/src/components/InitialMessage.tsx
+++ frontend-user/src/components/InitialMessage.tsx
@@ -1,44 +1,35 @@
-import { Card, CardDescription, CardHeader } from "./ui/card";
+import Topbar from "./Topbar";
+import { Card } from "./ui/card";
 
 const InitialMessage = () => {
+  const messages = [
+    "I'm a chatbot designed to answer your school-related questions.",
+    "Need information on a specific subject or questions about your school's policies? Just ask!",
+    "I'm available 24/7 to provide you with reliable information.",
+    "I'm constantly learning and improving. If I don't have an answer, I'll do my best to find it for you!",
+  ];
+
   return (
-    <div className="flex justify-center flex-col items-center space-y-4">
-      <img src="./ucnianguidebotlogo.svg" />
-      <h1 className="font-semibold text-4xl text-[#2b4f7e]">
-        UCnian Guide Bot
-      </h1>
-      <div className="flex justify-center items-center gap-4 opacity-[0.85] font-medium text-justify">
-        <Card className="max-w-[300px]">
-          <CardHeader>
-            <CardDescription className="text-base text-slate-900">
-              A chatbot designed to answer your school-related questions.
-            </CardDescription>
-          </CardHeader>
-        </Card>
-        <Card className="max-w-[350px]">
-          <CardHeader>
-            <CardDescription className="text-base text-slate-900">
-              Want to know more about a particular subject? Or do you have
-              questions about your school's policies or procedures? Just ask me!
-            </CardDescription>
-          </CardHeader>
-        </Card>
-        <Card className="max-w-[300px]">
-          <CardHeader>
-            <CardDescription className="text-base text-slate-900">
-              I'm available 24/7 to provide you with accurate and reliable
-              information.
-            </CardDescription>
-          </CardHeader>
-        </Card>
-        <Card className="max-w-[350px]">
-          <CardHeader>
-            <CardDescription className="text-base text-slate-900">
-              I'm always learning and improving, so if I don't know the answer
-              to your question, I'll do my best to find it for you.
-            </CardDescription>
-          </CardHeader>
-        </Card>
+    <div className="h-full w-full flex flex-col ">
+      <Topbar />
+      <div className="h-full flex justify-center flex-col items-center space-y-4 ">
+        <img
+          className="w-[150px] md:w-[200px]"
+          src="./ucnianguidebotlogo.svg"
+        />
+        <h1 className="text-3xl font-semibold md:text-4xl text-white">
+          UCnian Guide Bot
+        </h1>
+        <div className="flex flex-wrap justify-center items-center gap-2 md:gap-4">
+          {messages.map((message, index) => (
+            <Card
+              key={index}
+              className="max-w-[220px] p-3 font-medium text-center"
+            >
+              <h1 className="text-sm text-black">{message}</h1>
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );

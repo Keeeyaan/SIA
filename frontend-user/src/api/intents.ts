35178e1586022089dff9c4c71cf891e98cb8c095
gochuicod <gochuicodarelleq@gmail.com>
2024-04-20 22:28:14 +0800

    Removed interface due to errors

--- frontend-user/src/api/intents.ts
+++ frontend-user/src/api/intents.ts
@@ -4,13 +4,7 @@ axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
-export interface IIntents {
-    tag: string;
-    patterns: string[];
-    frequency: number;
-}
-
 export const getIntents = async (token: string) => {
-    const response = await axios.get<IIntents>(`intents/public/`, { headers: { Authorization: `Bearer ${token}` } });
+    const response = await axios.get(`intents/public/`, { headers: { Authorization: `Bearer ${token}` } });
     return response.data;
 }

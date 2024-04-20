56a14f9f4e828e9cc9460ace7b1bcc59ce716fd0
gochuicod <gochuicodarelleq@gmail.com>
2024-04-20 22:28:47 +0800

    Added inquiry to store

--- frontend-user/src/store/index.ts
+++ frontend-user/src/store/index.ts
@@ -3,10 +3,15 @@ import { create } from "zustand";
 interface GlobalStore {
   sidebarIsClosed: boolean;
   setSidebarIsClosed: () => void;
+  inquiry: string;
+  setInquiry: (value: string) => void;
 }
 
 export const useStore = create<GlobalStore>((set) => ({
   sidebarIsClosed: false,
   setSidebarIsClosed: () =>
     set((state) => ({ sidebarIsClosed: !state.sidebarIsClosed })),
+  inquiry: "",
+  setInquiry: (value: string) =>
+    set({ inquiry: value })
 }));

dc6b5a88844dbe52f9344a5eb3d0a95c3c1f48de
keeeyaan <keanjieden@gmail.com>
2024-04-21 14:41:35 +0800

    update sidebar ui

--- frontend-user/src/components/Sidebar.tsx
+++ frontend-user/src/components/Sidebar.tsx
@@ -1,13 +1,13 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { Sun, Bug, Trash2, HelpCircle } from "lucide-react";
 import { setCookie } from "react-use-cookie";
 
-import { Card, CardFooter, CardHeader } from "./ui/card";
-import { Label } from "./ui/label";
-import { Button } from "./ui/button";
 import useFetchIntents from "@/hooks/useFetchIntents";
-import { useQueryClient } from "@tanstack/react-query";
+import { Card, CardFooter, CardHeader } from "./ui/card";
+import { Button } from "./ui/button";
 import { useStore } from "@/store";
 import { toast } from "./ui/use-toast";
+import { Separator } from "./ui/separator";
 
 const Sidebar = () => {
   const { data: intents } = useFetchIntents();
@@ -33,47 +33,59 @@ const Sidebar = () => {
   };
 
   return (
-    <Card className="hidden md:block rounded-none border-none w-[350px]">
+    <Card className="hidden md:block rounded-none border-none max-w-[310px]">
       <div className="flex h-screen flex-col justify-between bg-gradient-to-b from-yellow-200 via-yellow-100 to-yellow-50">
-        <CardHeader className="p-5 text-center text-slate-800">
-          <h1 className="mb-5">Frequently Asked Questions</h1>
-          <div className="flex flex-col mt-10 space-y-5 text-sm font-medium">
+        <CardHeader className="p-5 text-center">
+          <h1 className="mb-2 uppercase text-white p-2 font-semibold bg-[#214E87]">
+            Frequently Asked Questions
+          </h1>
+          <div className="flex flex-col space-y-1">
             {intents &&
               intents.map((item) => {
                 return (
-                  <Label
-                    className="flex items-center mr-5"
+                  <Button
                     key={item._id + item.patterns}
+                    className="flex justify-start items-center h-full w-full text-wrap text-start text-[13px] text-slate-800 py-3 hover:bg-yellow-100"
+                    variant="ghost"
+                    size="sm"
+                    onClick={() => handleFAQ(item.patterns)}
                   >
-                    <HelpCircle size={18} className=" w-6 h-6 flex-shrink-0" />
-                    <Button
-                      className="hover:underline text-wrap text-start"
-                      variant="link"
-                      onClick={() => handleFAQ(item.patterns)}
-                    >
-                      {item.patterns}
-                    </Button>
-                  </Label>
+                    <HelpCircle size={18} className="mr-4 flex-shrink-0" />
+                    {item.patterns}
+                  </Button>
                 );
               })}
           </div>
         </CardHeader>
-        <div className="space-y-3">
-          <CardFooter className="block space-y-3">
-            <Button className="justify-start bg-transparent hover:bg-yellow-50 text-black">
-              <Sun className="mr-3 w-6 h-6 flex-shrink-0" />
+        <div className="space-y-3 w-full">
+          <Separator className="bg-slate-900" />
+          <CardFooter className="block space-y-1">
+            <Button
+              disabled
+              className="flex justify-start w-full text-wrap text-start text-[13px] text-slate-800 py-6 hover:bg-yellow-100"
+              variant="ghost"
+              size="sm"
+            >
+              <Sun size={18} className="mr-4 flex-shrink-0" />
               Switch to light mode
             </Button>
-            <Button className="justify-start bg-transparent hover:bg-yellow-50 text-black">
-              <Bug className="mr-3 w-6 h-6 flex-shrink-0" />
+            <Button
+              disabled
+              className="flex justify-start w-full text-wrap text-start text-[13px] text-slate-800 py-6 hover:bg-yellow-100"
+              variant="ghost"
+              size="sm"
+            >
+              <Bug size={18} className="mr-4 flex-shrink-0" />
               Report any issue
             </Button>
             <Button
+              className="flex justify-start text-[13px] w-full text-wrap text-start text-slate-800 py-6 hover:bg-yellow-100"
+              variant="ghost"
+              size="sm"
               onClick={handleClearConversation}
-              className="justify-start bg-transparent hover:bg-yellow-50 text-black"
             >
-              <Trash2 className="mr-3 w-6 h-6 flex-shrink-0" />
-              Clear conversations
+              <Trash2 size={18} className=" mr-4 flex-shrink-0" />
+              Clear Conversation
             </Button>
           </CardFooter>
         </div>

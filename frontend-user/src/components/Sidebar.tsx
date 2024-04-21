9c312082bf8037ca646cf7acecbac7e760483b70
keeeyaan <keanjieden@gmail.com>
2024-04-21 12:00:31 +0800

    added hidden logic

--- frontend-user/src/components/Sidebar.tsx
+++ frontend-user/src/components/Sidebar.tsx
@@ -1,14 +1,7 @@
-import {
-  Sun,
-  Bug,
-  Trash2,
-  HelpCircle,
-  // ArrowUpRightFromSquare,
-} from "lucide-react";
+import { Sun, Bug, Trash2, HelpCircle } from "lucide-react";
 import { getCookie, setCookie } from "react-use-cookie";
 
 import { Card, CardFooter, CardHeader } from "./ui/card";
-import { Separator } from "./ui/separator";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import useFetchIntents from "@/hooks/useFetchIntents";
@@ -19,44 +12,51 @@ import useUpdateConversation from "@/hooks/useUpdateConversation";
 const Sidebar = () => {
   const { data: intents } = useFetchIntents();
 
-  const { mutate: createConversation, isPending: createIsPending } = useCreateConversation();
-  const { mutate: updateConversation, isPending: updateIsPending } = useUpdateConversation();
+  const { mutate: createConversation, isPending: createIsPending } =
+    useCreateConversation();
+  const { mutate: updateConversation, isPending: updateIsPending } =
+    useUpdateConversation();
 
   const handleOnClickFAQ = (value: string) => {
     const temp = { inquiry: value, kbs_version: "1.0" };
 
-    if (!getCookie("ucnian_guidebot_token")) createConversation(temp)
+    if (!getCookie("ucnian_guidebot_token")) createConversation(temp);
     else {
       updateConversation({
         ...temp,
         token: getCookie("ucnian_guidebot_token"),
       });
     }
-  }
+  };
 
   return (
-    <Card className="rounded-none border-none w-[350px]">
+    <Card className="hidden md:block rounded-none border-none w-[350px]">
       <div className="flex h-screen flex-col justify-between bg-gradient-to-b from-yellow-200 via-yellow-100 to-yellow-50">
         <CardHeader className="p-5 text-center text-slate-800">
           <h1 className="">Frequently Asked Questions</h1>
           <div className="flex flex-col mt-10 space-y-5 text-sm font-medium">
-            {
-              intents &&
-              intents.map((item:any) => {
+            {intents &&
+              intents.map((item) => {
                 return (
-                  <Label className="flex items-center mr-5" key={item.id + item.patterns}>
+                  <Label
+                    className="flex items-center mr-5"
+                    key={item._id + item.patterns}
+                  >
                     <HelpCircle size={18} className=" w-6 h-6 flex-shrink-0" />
-                    <Button className="hover:underline text-wrap text-start" variant="link" onClick={() => handleOnClickFAQ(item.patterns)} disabled={createIsPending || updateIsPending}>
+                    <Button
+                      className="hover:underline text-wrap text-start"
+                      variant="link"
+                      onClick={() => handleOnClickFAQ(item.patterns)}
+                      disabled={createIsPending || updateIsPending}
+                    >
                       {item.patterns}
                     </Button>
                   </Label>
-                )
-              })
-            }
+                );
+              })}
           </div>
         </CardHeader>
         <div className="space-y-3">
-          <Separator className="bg-black" />
           <CardFooter className="block space-y-3">
             <Button className="justify-start bg-transparent hover:bg-yellow-50 text-black">
               <Sun className="mr-3 w-6 h-6 flex-shrink-0" />
@@ -66,10 +66,6 @@ const Sidebar = () => {
               <Bug className="mr-3 w-6 h-6 flex-shrink-0" />
               Report any issue
             </Button>
-            {/* <Button className="justify-start bg-transparent hover:bg-yellow-50 text-black">
-          <ArrowUpRightFromSquare className="mr-3 w-6 h-6 flex-shrink-0" />
-          Got any recommendations?
-        </Button> */}
             <Button
               onClick={() => setCookie("ucnian_guidebot_token", "")}
               className="justify-start bg-transparent hover:bg-yellow-50 text-black"

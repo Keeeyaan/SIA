e49d8ad6a595b941cfe4dcfcd8b61ee418c35e1d
keeeyaan <keanjieden@gmail.com>
2024-04-22 23:05:37 +0800

    update overflow-y to auto

--- frontend-user/src/pages/Home.tsx
+++ frontend-user/src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <div
-        className={`${convo?.sequence.length && "overflow-y-scroll scrollbar-thumb-gray-700 scrollbar-thin scrollbar-track-gray-100"} flex items-center justify-center h-full w-full bg-no-repeat bg-center bg-[url('/uclogo.png')] bg-[#19375f]  `}
+        className={`${convo?.sequence.length && "overflow-y-auto scrollbar-thumb-gray-700 scrollbar-thin scrollbar-track-gray-100"} flex items-center justify-center h-full w-full bg-no-repeat bg-center bg-[url('/uclogo.png')] bg-[#19375f]  `}
       >
         {convo?.sequence.length ? (
           <Conversation convo={convo} />

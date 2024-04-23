cbbdeb0920f988788e7a51af4fe1cf0e68326336
keeeyaan <keanjieden@gmail.com>
2024-04-24 01:17:55 +0800

    removed set inquiry

--- frontend-user/src/hooks/useUpdateConversation.ts
+++ frontend-user/src/hooks/useUpdateConversation.ts
@@ -1,14 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { useToast } from "@/components/ui/use-toast";
-
 import { updateConversation } from "@/api/conversation";
-import { useStore } from "@/store";
 
 export const useUpdateConversation = () => {
   const { toast } = useToast();
   const queryClient = useQueryClient();
-  const { inquiry, setInquiry } = useStore();
 
   return useMutation({
     mutationKey: ["conversation"],
@@ -17,7 +14,6 @@ export const useUpdateConversation = () => {
       queryClient.invalidateQueries({
         queryKey: ["conversation"],
       });
-      setInquiry("");
     },
     // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     onError: (error: any) => {

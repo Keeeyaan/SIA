4ffc1beb607999d0146a5670644d8dd2137eeed8
gochuicod <gochuicodarelleq@gmail.com>
2024-04-23 11:38:48 +0800

    Added a select version feature

--- frontend-user/src/components/Sidebar.tsx
+++ frontend-user/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { useQueryClient } from "@tanstack/react-query";
-import { Sun, Bug, Trash2, HelpCircle } from "lucide-react";
+import { Sun, Bug, Trash2, HelpCircle, Bot } from "lucide-react";
 import { setCookie } from "react-use-cookie";
 
 import useFetchIntents from "@/hooks/useFetchIntents";
@@ -8,13 +8,24 @@ import { Button } from "./ui/button";
 import { useStore } from "@/store";
 import { toast } from "./ui/use-toast";
 import { Separator } from "./ui/separator";
-import { Label } from "./ui/label";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import useFetchVersions from "@/hooks/useFetchVersions";
 
 const Sidebar = () => {
   const { data: intents } = useFetchIntents();
+  const { data: versions } = useFetchVersions();
 
   const queryClient = useQueryClient();
-  const { setFAQ, setInquiry } = useStore();
+  const { setFAQ, setInquiry, setVersion, version } = useStore();
 
   const handleClearConversation = () => {
     setCookie("ucnian_guidebot_token", "");
@@ -33,28 +44,29 @@ const Sidebar = () => {
     setInquiry(value);
   };
 
+  const handleVersionSelect = (version: string) => setVersion(version)
+
   return (
     <Card className="hidden md:block rounded-none border-none max-w-[310px]">
       <div className="flex h-screen flex-col justify-between bg-gradient-to-b from-yellow-200 via-yellow-100 to-yellow-50">
         <CardHeader className="p-5 text-center text-slate-800">
-          <h1 className="mb-5">Frequently Asked Questions</h1>
+          <h1 className="mb-2 uppercase text-white p-2 font-semibold bg-[#214E87]">
+            Frequently Asked Questions
+          </h1>
           <div className="flex flex-col mt-10 space-y-5 text-sm font-medium">
             {intents &&
               intents.map((item: any) => {
                 return (
-                  <Label
-                    className="flex items-center mr-5"
-                    key={item.id + item.patterns}
+                  <Button
+                    key={item._id + item.patterns}
+                    className="flex justify-start items-center h-full w-full text-wrap text-start text-[13px] text-slate-800 py-3 hover:bg-yellow-100"
+                    variant="ghost"
+                    size="sm"
+                    onClick={() => handleFAQ(item.patterns)}
                   >
-                    <HelpCircle size={18} className=" w-6 h-6 flex-shrink-0" />
-                    <Button
-                      className="hover:underline text-wrap text-start"
-                      variant="link"
-                      onClick={() => handleFAQ(item.patterns)}
-                    >
-                      {item.patterns}
-                    </Button>
-                  </Label>
+                    <HelpCircle size={18} className="mr-4 flex-shrink-0" />
+                    {item.patterns}
+                  </Button>
                 );
               })}
           </div>
@@ -62,6 +74,24 @@ const Sidebar = () => {
         <div className="space-y-3 w-full">
           <Separator className="bg-slate-900" />
           <CardFooter className="block space-y-1">
+            <Select>
+              <SelectTrigger className="flex text-[13px] justify-start text-slate-800 py-6 bg-transparent border-none hover:bg-yellow-100">
+                <Bot className="mr-4 flex-shrink-0" />
+                <SelectValue placeholder={`UCGB ${version}`} />
+              </SelectTrigger>
+              <SelectContent className="bg-[#214E87]">
+                <SelectGroup>
+                  <SelectLabel className="text-white">UCGB Versions</SelectLabel>
+                  {
+                    versions && versions.available_versions.map((item: any) => {
+                      return (
+                        <SelectItem className="text-white" value={item} key={item} onClick={() => handleVersionSelect(item)}>UCGB {item}</SelectItem>
+                      )
+                    })
+                  }
+                </SelectGroup>
+              </SelectContent>
+            </Select>
             <Button
               disabled
               className="flex justify-start w-full text-wrap text-start text-[13px] text-slate-800 py-6 hover:bg-yellow-100"

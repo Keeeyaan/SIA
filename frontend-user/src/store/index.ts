365028070915f678667410f8489e486413fd192b
keeeyaan <keanjieden@gmail.com>
2024-04-27 17:42:27 +0800

    removed unnecessary code

--- frontend-user/src/store/index.ts
+++ frontend-user/src/store/index.ts
@@ -3,22 +3,14 @@ import { create } from "zustand";
 interface GlobalStore {
   sidebarIsClosed: boolean;
   setSidebarIsClosed: () => void;
-  inquiry: string;
-  setInquiry: (value: string) => void;
   FAQ: string;
   setFAQ: (value: string) => void;
-  version: string;
-  setVersion: (version: string) => void;
 }
 
 export const useStore = create<GlobalStore>((set) => ({
   sidebarIsClosed: false,
   setSidebarIsClosed: () =>
     set((state) => ({ sidebarIsClosed: !state.sidebarIsClosed })),
-  inquiry: "",
-  setInquiry: (value: string) => set({ inquiry: value }),
   FAQ: "",
   setFAQ: (value: string) => set({ FAQ: value }),
-  version: "",
-  setVersion: (version: string) => set({ version: version }),
 }));

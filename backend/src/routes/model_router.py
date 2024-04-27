76ee3ca2c6f80a0d07aec42de6580e964ca8da49
keeeyaan <keanjieden@gmail.com>
2024-04-27 14:26:08 +0800

    update get model search for models on bot_models path

--- backend/src/routes/model_router.py
+++ backend/src/routes/model_router.py
@@ -1,6 +1,6 @@
+import os
 from fastapi import APIRouter, status, Depends
 from fastapi.security import HTTPAuthorizationCredentials
-
 from src.utils.train import init, create_model, fit_model, save_model
 from src.utils.user import get_current_user
 
@@ -12,24 +12,28 @@ from datetime import datetime
 
 model = APIRouter()
 
+
 @model.get('/', status_code=status.HTTP_200_OK)
-async def get_latest_model():
-    all_kbs = await KnowledgeBase.find_all().to_list()
-    sorted_kbs = sorted(all_kbs,key=lambda x: x.created_at, reverse=True)
+async def get_models():
+    bot_models_directory = os.path.abspath(os.path.join(os.path.dirname(
+        os.path.dirname(os.path.dirname(__file__))), "bot_models"))
 
-    versions = [ kb.version for kb in sorted_kbs ]
+    if not os.path.exists(bot_models_directory):
+        os.makedirs(bot_models_directory)
 
-    return { "available_versions":  versions }
+    models = []
+
+    if len(os.listdir(bot_models_directory)) > 0:
+        for file in os.listdir(bot_models_directory):
+            if file.endswith('.keras'):
+                filename, _ = os.path.splitext(file)
+                models.append(filename)
+
+    return {"models":  models}
 
 
 @model.post('/', status_code=status.HTTP_200_OK)
-async def train_model(data: TrainModel, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> dict:
-    intents = await Intent.find_all().to_list()
-
-    create = KnowledgeBase(intents=intents, version=data.kbs_version, created_at=datetime.now())
-
-    await create.create()  
-
+async def create_model(data: TrainModel, current_user: HTTPAuthorizationCredentials = Depends(get_current_user)) -> dict:
     kbs = await KnowledgeBase.find_one(KnowledgeBase.version == data.kbs_version)
 
     initial = init({"intents": kbs.intents})
@@ -48,4 +52,4 @@ async def train_model(data: TrainModel, current_user: HTTPAuthorizationCredentia
 
     save_model(model, f"version_{data.kbs_version}", "keras")
 
-    return {"detail": "Model updated successfully!"}
+    return {"detail": "Model created successfully!"}

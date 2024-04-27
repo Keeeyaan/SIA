1f45a531e054f48d6971d1f0830dbd28873d6582
keeeyaan <keanjieden@gmail.com>
2024-04-27 15:16:38 +0800

    removed name

--- frontend-admin/src/api/model.ts
+++ frontend-admin/src/api/model.ts
@@ -1,10 +1,7 @@
 import { axiosPrivate } from "@/components/AxiosInterceptor";
 import { IGenericResponse } from "./intents";
 
-export const createModel = async (data: {
-  name: string;
-  kbs_version: string;
-}) => {
+export const createModel = async (data: { kbs_version: string }) => {
   const response = await axiosPrivate.post<IGenericResponse>(`model/`, data);
   return response.data;
 };

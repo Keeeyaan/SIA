92abaa2ec9ba943660e6931ce10e926e5cf5642c
keeeyaan <keanjieden@gmail.com>
2024-04-27 15:30:14 +0800

    fixed import

--- frontend-user/src/api/feedback.ts
+++ frontend-user/src/api/feedback.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
-import { Sequence } from "./conversation";
+import { ISequence } from "./conversation";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post["Content-Type"] = "application/json";
 
 interface Feedback {
-  sequence: Sequence;
+  sequence: ISequence;
   comment: string;
   sentiment: string;
   version: string;

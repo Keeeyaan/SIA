589ec3a9c6b288c65a4c0f607ee7447ad65067c8
keeeyaan <keanjieden@gmail.com>
2024-04-29 12:39:27 +0800

    added the knowledge base snapshot

--- frontend-admin/src/pages/KnowledgeBase.tsx
+++ frontend-admin/src/pages/KnowledgeBase.tsx
@@ -1,69 +1,65 @@
 import { useState } from "react";
 
 import Wrapper from "@/components/Wrapper";
-import useFetchKnowledgeBase from "@/hooks/useFetchKnowledgeBase";
-import SelectModelVersionButton from "@/components/SelectModelVersionButton";
-import TablePTRP from "@/components/TablePTRP";
-
 import { Card, CardHeader } from "@/components/ui/card";
-import SelectTagButton from "@/components/SelectTagButton";
-import { IGetIntentsResponse } from "@/api/intents";
 
-const KnowledgeBase = () => {
+import useFetchIntents from "@/hooks/useFetchIntents";
+import useFetchPTandRPByTag from "@/hooks/useFetchPTandRPByTag";
+import TablePTRP from "@/components/TablePTRP";
+import CreateIntentButton from "@/components/CreateIntentButton";
+import SelectTagButton from "@/components/SelectTagButton";
+import AddPatternButton from "@/components/AddPatternButton";
+import AddResponseButton from "@/components/AddResponseButton";
+import CreateKnowledgeBaseButton from "@/components/CreateKnowledgeBase";
+
+const Intents = () => {
   const [value, setValue] = useState("");
-  const [valueTag, setValueTag] = useState("");
-  const [tagValue, setTagValue] = useState<IGetIntentsResponse | undefined>();
-  const [specKBS, setSpecKBS] = useState<IGetIntentsResponse[] | []>([]);
-  const { data: kbs, isLoading } = useFetchKnowledgeBase();
+
+  const { data: intents, isLoading } = useFetchIntents();
+  const { data: PTRP, isLoading: isLoadingPTRP } = useFetchPTandRPByTag(value);
 
   return (
-    <Wrapper title="Knowledge Base" norMargin>
-      <div className="flex items-center gap-4">
-        <SelectModelVersionButton
-          kbs={kbs}
-          isLoading={isLoading}
-          value={value}
-          setValue={setValue}
-          setSpecKBS={setSpecKBS}
-          setTagValue={setTagValue}
-        />
-        <SelectTagButton
-          intents={specKBS}
-          isLoading={isLoading}
-          value={valueTag}
-          setValue={setValueTag}
-          setTagValue={setTagValue}
-          noAction
-        />
+    <Wrapper norMargin title="Intents">
+      <div className="mb-4 flex justify-between items-center">
+        <div>
+          <CreateIntentButton />
+          <SelectTagButton
+            intents={intents}
+            isLoading={isLoading}
+            value={value}
+            setValue={setValue}
+          />
+        </div>
+        <CreateKnowledgeBaseButton />
       </div>
       <Card className="mt-4">
         <CardHeader>
           <div className="flex items-center gap-2">
             <h1 className="font-semibold">Patterns</h1>
+            <AddPatternButton value={value} />
           </div>
           <div className="w-full h-[500px] border rounded overflow-scroll p-4">
             <TablePTRP
-              isLoadingPTRP={isLoading}
-              PTRP={tagValue?.patterns}
+              isLoadingPTRP={isLoadingPTRP}
+              PTRP={PTRP?.patterns}
               itemHead="Pattern"
               caption="A list of pattern."
               tag={value}
-              noAction
             />
           </div>
         </CardHeader>
         <CardHeader>
           <div className="flex items-center gap-2">
             <h1 className="font-semibold">Responses</h1>
+            <AddResponseButton value={value} />
           </div>
           <div className="w-full h-[500px] border rounded overflow-scroll p-4">
             <TablePTRP
-              isLoadingPTRP={isLoading}
-              PTRP={tagValue?.responses}
+              isLoadingPTRP={isLoadingPTRP}
+              PTRP={PTRP?.responses}
               itemHead="Response"
               caption="A list of responses."
               tag={value}
-              noAction
             />
           </div>
         </CardHeader>
@@ -72,4 +68,4 @@ const KnowledgeBase = () => {
   );
 };
 
-export default KnowledgeBase;
+export default Intents;
